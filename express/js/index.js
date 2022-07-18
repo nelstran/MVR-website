d65e846015408fe5898d010a45874204c8779d16
nelson tran <nelsonquoc2003@yahoo.com>
2022-07-18 17:10:29 -0600

    Added custom color picker

--- express/js/index.js
+++ express/js/index.js
@@ -1,69 +1,63 @@
-var cssVar = window.getComputedStyle(document.body);
+// var cssVar = window.getComputedStyle(document.body);
 
+//#region SETUP
 $(window).resize(setup);
-
-document.getElementById("topSide").addEventListener("wheel", function(e){
-  // prevent the default scrolling event
-  e.preventDefault();
-
-  // scroll the div
-  document.getElementById("main").scrollBy(e.deltaX, e.deltaY);
-});
-
-// var didScroll;// on scroll, let the interval function know the user has scrolled
-
-// $(window).scroll(function(event){
-//     didScroll = true;
-// });
-
-// setInterval(function() {
-//     if (didScroll) {
-//       hasScrolled();
-//       didScroll = false;
-//     }
-// }, 100);
-
-// var scrollPos = 0;
-
-// function hasScrolled() {
-//     // console.log($(window).scrollTop() <= scrollPos);
-//     if($(window).scrollTop() <= scrollPos){
-//         $("#topSide").css("position","fixed");
-//         $("#topSide").css("top","0px");
-
-//         // $("#topSide").animate({top:"0px"}, 100);
-//     }
-//     else{
-//         $("#topSide").css("position","static");
-//         $("#topSide").css("top",`-${cssVar.getPropertyValue('--ts-Height')}`);
-//         // $("#topSide").animate({top:`-${cssVar.getPropertyValue('--ts-Height')}`}, 100);
-//     }
-//     scrollPos = $(window).scrollTop();
-// }
 
 function setup(){
     setIMGSize();
+    setColorValue();
 }
+
+function setColorValue(){
+    $("#bg1").val(rgbToHex($(":root").css('--bg')));
+    $("#promoBg").val(rgbToHex($(":root").css('--promo-bg')));
+    $("#tsBg").val(rgbToHex($(":root").css('--ts-bg')));
+    $("#fontColor").val(rgbToHex($(":root").css('--font-color')));
+};
+
 function setIMGSize(){
     $(".promoIMG").each(function(){
         var width = $(this).width();
         $(this).css("height", width);
-
     })
-    
-    if($(window).height() > $(window).width())
-        return;
-    
-    $(".event").each(function(){
-        // var totalHeight = 0;
-        // $(this).children().each(function(){
-        //     totalHeight = totalHeight + $(this).outerHeight(true);
-        //     console.log($(this).outerHeight(true));
-        // });
-        // console.log(totalHeight);
-        // $(this).css("min-height", totalHeight+"px");
-    });
 };
+
+//#endregion
+
+//#region HELP FUNCTIONS
+function rgbToHex(rgb) {
+    var a = rgb.split("(")[1].split(")")[0];
+    a = a.split(",");
+    var b = a.map(function(x){             //For each array element
+        x = parseInt(x).toString(16);      //Convert to a base16 string
+        return (x.length==1) ? "0"+x : x;  //Add zero if we get only one character
+    })
+    return "#"+b.join("");
+  }
+//#endregion
+
+//#region EVENT LISTENER
+document.getElementById("topSide").addEventListener("wheel", function(e){
+    // prevent the default scrolling event
+    e.preventDefault();
+
+    // scroll the div
+    document.getElementById("main").scrollBy(e.deltaX, e.deltaY);
+});
+  
+$("#bg1").on("change", function(){
+    $(":root").css('--bg', $(this).val());
+});
+$("#promoBg").on("change", function(){
+    $(":root").css('--promo-bg', $(this).val());
+});
+$("#tsBg").on("change", function(){
+    $(":root").css('--ts-bg', $(this).val());
+});
+$("#fontColor").on("change", function(){
+    $(":root").css('--font-color', $(this).val());
+})
+//#endregion
 
 //THIS IS A TEST DELETE WHEN NEEDED
 // $(document).ready(function() {
@@ -76,5 +70,3 @@ function setIMGSize(){
 //             }
 //         });
 // }); 
-
-// const output = document.querySelector("#output");
