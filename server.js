72c9a573792190eb74cb7020360b64325c7a6e67
nelson tran <nelsonquoc2003@yahoo.com>
2022-07-20 18:45:31 -0600

    Added templates

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
-const http = require('http');
+// const http = require('http');
 const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.json());
 app.use(express.static("express"));// default URL for website
-app.use('/', function(req,res){
-  res.sendFile(path.join(__dirname+'/express/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
-const server = http.createServer(app);
+
+// app.use('/', function(req,res){
+//   res.sendFile(path.join(__dirname+'/express/index.html'));
+//   //__dirname : It will resolve to your project folder.
+// });
+// const server = http.createServer(app);
+// const port = process.env.PORT || 3000;
+// server.listen(port);
+// console.debug('Server listening on port ' + port);
+
+// const express = require('express');
+// const app = express();
 const port = process.env.PORT || 3000;
-server.listen(port);
-console.debug('Server listening on port ' + port);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+app.get('/home', (req, res) => {
+  res.render('home');
+});
+app.get('/pages/projects', (req, res) => {
+  res.render('pages/projects');
+});
+app.listen(port, () => {
+  console.log(`App listening at port ${port}`);
+});
