3b6664329aa745c4fd6fdbe1735ff9c6366e05fd
nelson tran <nelsonquoc2003@yahoo.com>
2022-07-26 17:05:59 -0600

    Heroku Database

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // const http = require('http');
 const express = require('express');
 const path = require('path');
+const env = require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 const { Client } = require('pg');
+const { CLIENT_RENEG_WINDOW } = require('tls');
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: {
@@ -11,14 +13,16 @@ const client = new Client({
   }
 });
 
+var events;
+var updateEvents = function(){
+  client.query("SELECT * FROM events", (err, data) =>{
+    if (err) throw err;
+    events = data.rows;
+  })
+};
+
 client.connect();
-client.query("SELECT * FROM events", (err, res) =>{
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-})
+
 app.use(express.json());
 app.use(express.static("express"));// default URL for website
 
@@ -26,25 +30,31 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
 
 app.get('/', (req, res) => {
-    res.render('home');
+  updateEvents();
+  res.render('home', {events: events});
 });
 app.get('/home', (req, res) => {
-  res.render('home');
+  updateEvents();
+  res.render('home', {events: events});
 });
 app.get('/pages/projects', (req, res) => {
-  res.render('pages/projects');
+  updateEvents();
+  res.render('pages/projects', {events: events});
 });
 app.get('/pages/social', (req, res) => {
-  res.render('pages/social');
+  updateEvents();
+  res.render('pages/social', {events: events});
 });
 app.get('/pages/forum', (req, res) => {
-  res.render('pages/forum');
+  updateEvents();
+  res.render('pages/forum', {events: events});
 });
 app.get('/pages/about', (req, res) => {
-  res.render('pages/about');
+  updateEvents();
+  res.render('pages/about', {events: events});
 });
 app.get('/pages/join', (req, res) => {
-  res.render('pages/join');
+  res.render('pages/join', {events: events});
 });
 app.listen(port, () => {
   console.log(`App listening at port ${port}`);
