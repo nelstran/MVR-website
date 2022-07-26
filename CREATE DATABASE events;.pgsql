eac89c53e2b8a1b6a17fca3be581ead2161b9831
nelson tran <nelsonquoc2003@yahoo.com>
2022-07-26 15:39:44 -0600

    Heroku

---
+++ CREATE DATABASE events;.pgsql
@@ -0,0 +1,18 @@
+
+-- CREATE TABLE events (
+--     event_id serial PRIMARY KEY,
+--     event_name varchar(255) NOT NULL,
+--     event_date date NOT NULL,
+--     event_location varchar(255) NOT NULL,
+--     event_promo varchar(255)
+-- );
+
+-- INSERT INTO events(event_name, event_date, event_location, event_promo)
+-- VALUES(
+--     'Habo',
+--     '12/01/00',
+--     'Outside',
+--     '/images/habo.png'
+-- )
+
+SELECT * from events
--- server.js
+++ server.js
@@ -2,21 +2,25 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 3000;
+const { Client } = require('pg');
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+client.query("SELECT * FROM events", (err, res) =>{
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+})
 app.use(express.json());
 app.use(express.static("express"));// default URL for website
-
-// app.use('/', function(req,res){
-//   res.sendFile(path.join(__dirname+'/express/index.html'));
-//   //__dirname : It will resolve to your project folder.
-// });
-// const server = http.createServer(app);
-// const port = process.env.PORT || 3000;
-// server.listen(port);
-// console.debug('Server listening on port ' + port);
-
-// const express = require('express');
-// const app = express();
-const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
