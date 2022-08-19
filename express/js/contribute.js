2549cc274cbd51c472061afcb7993c1aa1ba48e1
nelson tran <nelsonquoc2003@yahoo.com>
2022-08-18 19:40:49 -0600

    Added to join form

--- express/js/contribute.js
+++ express/js/contribute.js
@@ -1,7 +1,13 @@
-$("#SSN input").keypress(function (event){
+$("#SSN input, #credit input").keypress(function (event){
     return (event.charCode !=8 && event.charCode ==0 || (event.charCode >= 48 && event.charCode <= 57));
-})
-
+});
+$("#CC").keyup(function(event){
+    var text = $(this).val().split(" ").join("");
+    if(text.length > 0){
+        text = text.match(new RegExp('.{1,4}', 'g')).join(" ");
+    }
+    $(this).val(text);
+});
 $("#SSN input").keyup(function (event) {
     if (this.value.length == this.maxLength)
         $(this).next('#SSN input').focus();
@@ -11,9 +17,18 @@ $("#SSN input").keyup(function (event) {
 $("#joinButton").click(function(e){
     if($("#first").val().length > 0 &&
         $("#last").val().length > 0 &&
+        $("#mail").val().length > 0 &&
         $("#mail").is(":valid")){
             $("form > *").css("display", "none");
             $("form > h1").css("display", "block");
     }
+    else{
+        $("label[for=first]").addClass("animate");
+        $("#require").css("display", "block");
+    }
     return false;
 });
+
+$("label[for=first]").on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function (){
+    $(this).removeClass("animate");
+})
