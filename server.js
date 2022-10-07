6f1edb8f47b237f9c68d561ea7f2508606bc1dc5
nelson tran <nelsonquoc2003@yahoo.com>
2022-10-06 21:05:54 -0600

    Saving server

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require('path');
 const env = require('dotenv').config();
 const app = express();
 const session = require('express-session');
-const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 3000;
 const { Client } = require('pg');
 const client = new Client({
