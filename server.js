933b603d5eecb708e84e3d186cfaceba78de9045
nelson tran <nelsonquoc2003@yahoo.com>
2022-10-06 18:42:28 -0600

    Added login

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const env = require('dotenv').config();
 const app = express();
+const session = require('express-session');
 const port = process.env.PORT || 3000;
 const { Client } = require('pg');
 const client = new Client({
@@ -14,6 +15,7 @@ const client = new Client({
 
 var events;
 var projects;
+
 function updateEvents(){
   client.query("SELECT * FROM events ORDER BY event_date ASC", (err, data) =>{
     if (err) return events = null;
@@ -30,7 +32,13 @@ client.connect()
 .then(() => console.log("Connected to Postgresql server!"))
 .catch(err => console.error("Unable to connect to Postgresql server!"));
 
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("express"));// default URL for website
 
 app.set('view engine', 'ejs');
@@ -70,6 +78,46 @@ app.get('/pages/join', (req, res) => {
 app.get('/pages/contact', (req, res) => {
   res.render('pages/contact', {events: events});
 });
+
+app.get('/pages/wvcdaboys', (req, res) => {
+  updateEvents();
+  res.render('pages/wvcdaboys', {events: events});
+});
 app.listen(port, () => {
   console.log(`App listening at port ${port}`);
 });
+
+app.post('/auth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		client.query(`SELECT * FROM users WHERE user_id='${username.trim()}' AND password='${password.trim()}'`, function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error)
+        throw error;
+			// If the account exists
+			if (results.rows.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.username = username;
+				// Redirect to home page
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+// Handling Error
+process.on("unhandledRejection", err => {
+  console.log(`An error occurred: ${err.message}`)
+  server.close(() => process.exit(1))
+})
