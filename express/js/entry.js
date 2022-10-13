c64db1a623e8e1bf6c4353ae729b23b18e172764
nelson tran <nelsonquoc2003@yahoo.com>
2022-10-12 18:49:33 -0600

    Markdown to HTML preview

--- express/js/entry.js
+++ express/js/entry.js
@@ -10,6 +10,20 @@ var simplemde = new SimpleMDE({
 		delay: 1000,
 	},
     status: ["autosave"],
-    spellChecker: false 
-}
-);
+    spellChecker: false
+});
+var converter = new showdown.Converter();
+$("#previewButton").click(function(){
+    let entry = converter.makeHtml(simplemde.value());
+    let title = $(`<h1> ${$("#titleInput").val()} </h1>`);
+    let dt = new Date();
+    let hours = dt.getHours() % 12;
+    let ampm = dt.getHours() > 12 ? "pm" : "am"; 
+    let timestamp = `${dt.getMonth()}/${dt.getDay()}/${dt.getFullYear()} ${hours}:${dt.getMinutes()} ${ampm}`;
+    let date = $(`<p class="date"> ${timestamp}</p>`);
+    $("#preview").empty();
+    $("#preview").append(title);
+    $("#preview").append(date);
+    
+    $("#preview").append(entry);
+})
