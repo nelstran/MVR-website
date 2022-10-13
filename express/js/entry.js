0b4c543b515b64b5d8f42dbefc9bc2df77c262ee
nelson tran <nelsonquoc2003@yahoo.com>
2022-10-13 16:15:20 -0600

    Improved entry functionality

--- express/js/entry.js
+++ express/js/entry.js
@@ -14,16 +14,30 @@ var simplemde = new SimpleMDE({
 });
 var converter = new showdown.Converter();
 $("#previewButton").click(function(){
+    $("#preview").removeClass("preview");
     let entry = converter.makeHtml(simplemde.value());
     let title = $(`<h1> ${$("#titleInput").val()} </h1>`);
+    let imgDiv = $(`<div class="home-img-container"></div>`);
+    let img = $(`<img src="/images/dog.webp">`);
+    imgDiv.append(img);
     let dt = new Date();
     let hours = dt.getHours() % 12;
     let ampm = dt.getHours() > 12 ? "pm" : "am"; 
-    let timestamp = `${dt.getMonth()}/${dt.getDay()}/${dt.getFullYear()} ${hours}:${dt.getMinutes()} ${ampm}`;
+    // let timestamp = `${dt.getMonth()}/${dt.getDay()}/${dt.getFullYear()} ${hours}:${dt.getMinutes()} ${ampm}`;
+    let timestamp = dt.toLocaleString([], {
+        year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit'
+    });
     let date = $(`<p class="date"> ${timestamp}</p>`);
     $("#preview").empty();
     $("#preview").append(title);
     $("#preview").append(date);
-    
+    $("#preview").append(imgDiv);
     $("#preview").append(entry);
+
+    $('html, body').animate({
+        scrollTop: $(`#preview`).offset().top - 50
+    }, 500);
+    $('#main').animate({
+        scrollTop: $("#main").scrollTop() + ($("#preview").offset().top - $("#main").offset().top)
+    }, 500);
 })
